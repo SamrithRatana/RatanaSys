7b2ecaf34794003e8caf460f480c3c1ce52edad3
DESKTOP-NDP40NN\Chhunlay <yourkchhunlay188@gmail.com>
2026-05-11 13:47:52 +0700

    final deploy app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
 import type { Metadata } from "next";
+import Script from "next/script"; // Import this
 import "./globals.css";
 import Providers from "./Provider";
-import { Toaster } from "sonner"; // ← add this import
+import { Toaster } from "sonner";
+
 export const metadata: Metadata = {
   title: "CAM LMS",
   description: "Leave Management System by Camprotec",
+  // --- PWA SETTINGS ---
+  manifest: "/manifest.json", 
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "CAM LMS",
+  },
+  // --------------------
 };
 
 export default function RootLayout({
@@ -14,6 +24,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Load Telegram SDK */}
+        <Script 
+          src="https://telegram.org/js/telegram-web-app.js" 
+          strategy="beforeInteractive" 
+        />
+      </head>
       <body className="">
         <Providers
           attribute="class"
@@ -22,7 +39,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <Toaster richColors position="top-right" /> {/* ← add this */}
+          <Toaster richColors position="top-right" />
         </Providers>
       </body>
     </html>
